48be2e1ff0774b4a8620a6d2f7e8661f3a8220fc
Mitsu <mitsk2002@gmail.com>
2026-02-11 16:12:08 -0600

    Prepare for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './App.module.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio-blog">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
